68b3611012dfce89c119b402acf90858801d3866
Otilio <otiliojr1999@gmail.com>
2024-03-04 15:04:13 -0600

    Project completed

---
+++ src/main.jsx
@@ -0,0 +1,28 @@
+import React, { useState } from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+// import StarRating from "./StarRating.jsx";
+
+// function Test() {
+//   const [movieRating, setMovieRating] = useState(0);
+
+//   return (
+//     <div>
+//       <StarRating color="blue" maxRating={10} onSetRating={setMovieRating} />
+//       <p>This movie was rated {movieRating} starts</p>
+//     </div>
+//   );
+// }
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <App />
+    {/* <StarRating
+      maxRating={5}
+      messages={["Terrible", "Bad", "Okay", "Good", "Amazing"]}
+    />
+    <StarRating size={24} color="red" defaultRating={2} />
+    <Test /> */}
+  </React.StrictMode>
+);
